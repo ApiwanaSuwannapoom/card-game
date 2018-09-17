d3d3807fd7396f6dae3b04f9b667d5877cb82c5f
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2018-09-18 00:18:51 +0700

    Styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
-const word = "Hello";
+const word = "hello";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)}
+       {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
       </div>
+      
     );
   }
 }
