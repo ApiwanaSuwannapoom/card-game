871f1fe1a90adc07cc68130c6dc31db8e6e3aaae
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2018-09-21 01:01:17 +0700

    Handler as Props

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import CharacterCard from './CharacterCard';
-export default class WordCard extends
-Component {
-render() {
- return (
- <div>
-{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
- </div>
- );
- }
-}
+import CharacterCard from "./CharacterCard";
+
+export default class WordCard extends Component {
+    
+        render(){	    
+            return(	       
+                <div>	            
+                    {
+                    Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler} />)
+                    }
+                </div>	           
+            );	       
+        }	   
+        activationHandler = c => { console.log(`${c} has been activated.`)}
+    }    
+
